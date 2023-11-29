5eb682ef2f5dd06c4d35eaad08cf5d6cc5d1a26b
johowardmcc <79470034+johowardmcc@users.noreply.github.com>
2023-11-29 12:33:32 +0000

    Add files via upload

---
+++ RunMosaic.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#PBS -N Mosaic
+#PBS -l mem=30gb
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=50:00:00
+#PBS -j oe
+
+# OPTIONAL: record some potentially useful details about the job:
+echo Running on host `hostname`
+echo Start time is `date`
+echo Directory is `pwd`
+echo PBS job ID is $PBS_JOBID
+
+#load your program if it is installed globally
+module load languages/R-3.5.1-ATLAS-gcc-6.1
+
+# set a local temporary folder
+export TMPDIR=$HOME/.local
+
+# Set paths to relevant directories
+export WD=/newhome/jh17379/CHAPTER5/6_MOSAIC
+
+cd $WD
+
+# Run mosaic
+#Rscript RunMosaic.R
+Rscript RunMosaic2.R
+
+echo End time is `date`
---
+++ RunMosaic2.R
@@ -0,0 +1,9 @@
+# March2021
+
+require(MOSAIC)
+
+args = commandArgs(trailingOnly=TRUE)
+
+# Run MOSAIC (all chromosomes)
+mosaic.result=run_mosaic("scot","/newhome/jh17379/CHAPTER5/6_MOSAIC/INPUT2/",1:19,A=2,NUMI=36)
+
